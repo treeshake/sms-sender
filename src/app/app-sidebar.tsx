d0d55a0a5eab65941e108c304c5de52209552efb
Danh Nguyen <software@treeshake.com.au>
2024-11-04 22:02:58 +1100

    feat(app): Add sidebar

---
+++ src/app/app-sidebar.tsx
@@ -0,0 +1,52 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@treeshake/ui/components/sidebar";
+
+import { Home } from "lucide-react";
+import { FooterContent } from "./footer-content";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>SMS Sender</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <FooterContent />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
---
+++ src/app/footer-content.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "@treeshake/ui/components/mode-toggle";
+
+export function FooterContent() {
+  return (
+    <div className="flex items-center justify-center gap-2">
+      <small>© {`${new Date().getFullYear()}`} Purist Studios</small>
+      <ModeToggle />
+    </div>
+  );
+}
